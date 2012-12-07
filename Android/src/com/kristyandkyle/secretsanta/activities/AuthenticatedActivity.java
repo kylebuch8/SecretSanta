3a78d1f37a6e2ef4182b17627250a7318d02d2f6
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2012-12-07 09:51:28 -0500

    added a check for authentication when an authenticated activity resumes

--- Android/src/com/kristyandkyle/secretsanta/activities/AuthenticatedActivity.java
+++ Android/src/com/kristyandkyle/secretsanta/activities/AuthenticatedActivity.java
@@ -1,13 +1,29 @@
 package com.kristyandkyle.secretsanta.activities;
 
 import com.example.android.actionbarcompat.ActionBarActivity;
+import com.kristyandkyle.secretsanta.SecretSantaApplication;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AuthenticatedActivity extends ActionBarActivity {
 
+	private SecretSantaApplication app;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		if (!app.isAuthenticated()) {
+			Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+			startActivity(intent);
+			finish();
+		}
+	}	
 
 }
