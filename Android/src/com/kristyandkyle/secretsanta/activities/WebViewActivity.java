4217496d2bf9eeb0ba5b2b80d8ab64910e214861
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2012-12-11 13:13:59 -0500

    got the webview to work

--- Android/src/com/kristyandkyle/secretsanta/activities/WebViewActivity.java
+++ Android/src/com/kristyandkyle/secretsanta/activities/WebViewActivity.java
@@ -1,9 +1,14 @@
 package com.kristyandkyle.secretsanta.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends Activity {
 
@@ -15,6 +20,44 @@ public class WebViewActivity extends Activity {
 		
 		WebView webView = new WebView(this);
 		setContentView(webView);
+		
+		// enable javascript
+		webView.getSettings().setJavaScriptEnabled(true);
+		
+		webView.setWebChromeClient(new WebChromeClient() {
+
+			@Override
+			public void onProgressChanged(WebView view, int newProgress) {
+				// TODO Auto-generated method stub
+				super.onProgressChanged(view, newProgress);
+			}
+			
+		});
+		
+		webView.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				setTitle(url);
+				
+				String host = Uri.parse(url).getHost();
+				if (host.equalsIgnoreCase("localhost")) {
+					String code = Uri.parse(url).getQueryParameter("code");
+					
+					Intent result = new Intent();
+					result.putExtra("code", code);
+					setResult(RESULT_OK, result);
+					finish();
+				}
+			}
+			
+		});
+		
+		// load the web page
+		Intent intent = getIntent();
+		if (intent.getData() != null) {
+			webView.loadUrl(intent.getDataString());
+		}
 	}
 	
 }
